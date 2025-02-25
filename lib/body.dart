d7f81399562396d01c7d044a732a9fca09b2883d
jamilhasan3301 <hasan15-5222@diu.edu.bd>
2025-02-25 12:32:13 +0600

    navigation

---
+++ lib/body.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppaBody extends StatefulWidget {
+  const AppaBody({super.key});
+
+  @override
+  State<AppaBody> createState() => _AppaBodyState();
+}
+
+class _AppaBodyState extends State<AppaBody> {
+  int currentIndex = 1;
+  final List<Widget> pages = [
+    Text("Home"),
+    Text("profile"),
+  ];
+  void onClickMENU(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onClickMENU,
+        currentIndex: currentIndex,
+        items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.navigation), label: "navigation"),
+      ]),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/body.dart';
 
 void main() {
   runApp(const Lab06AdvanceUI());
@@ -13,21 +14,23 @@ class Lab06AdvanceUI extends StatelessWidget {
     return MaterialApp(
         title: "Advance Flutter UI",
         home: Scaffold(
-          bottomNavigationBar: BottomNavigationBar(
-            items:[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),label: "home"),
-               BottomNavigationBarItem(
-                icon: Icon(Icons.navigation),label: "navigation"),  
-            ] ),
           drawer: Drawer(
             child: ListView(
               children: [
-                DrawerHeader(child: Row(
+                DrawerHeader(
+                    child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Icon(Icons.settings,size: 25,color: Colors.black,),
-                    Icon(Icons.access_alarm_sharp,size: 25,color: Colors.blueAccent,)
+                    Icon(
+                      Icons.settings,
+                      size: 25,
+                      color: Colors.black,
+                    ),
+                    Icon(
+                      Icons.access_alarm_sharp,
+                      size: 25,
+                      color: Colors.blueAccent,
+                    )
                   ],
                 )),
                 ListTile(
@@ -46,24 +49,7 @@ class Lab06AdvanceUI extends StatelessWidget {
           appBar: AppBar(
             title: Text("ADvance UI"),
           ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                CachedNetworkImage(
-                  imageUrl:
-                      "https://images.unsplash.com/photo-1740219148636-824ab17bdd57?q=80&w=1998&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                  placeholder: (context, url) => CircularProgressIndicator(),
-                  errorWidget: (context, url, error) => Icon(Icons.error),
-                ),
-                CachedNetworkImage(
-                  imageUrl:
-                      "https://plus.unsplash.com/premium_photo-1717529138029-5b049119cfb1?q=80&w=1994&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                  placeholder: (context, url) => CircularProgressIndicator(),
-                  errorWidget: (context, url, error) => Icon(Icons.error),
-                ),
-              ],
-            ),
-          ),
+          body: AppaBody(),
         ));
   }
 }
